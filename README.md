68579e2291faaa43f659b340cf26e6ea48149468
Joshua Hellerman <joshuahellerman@gmail.com>
2024-09-18 18:36:58 -0700

    Removed all files from the repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sentiment-Miner
-A dynamic, customizable sentiment mining tool designed for finance. 
--- utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def ensure_directory(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
